e3ce391f707591ab5e5e6986ce1f62baea752b4c
6519N10003 <s6519N10003@sau.ac.th>
2023-06-15 14:34:19 +0700

    Commit versions 1.......

---
+++ ep01/Practice01.c
@@ -0,0 +1,18 @@
+// ประกาศ libary
+//workshop01.c
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+
+	printf ("*****************************************\n");
+	printf ("*                                       *\n");
+	printf ("*                6519N10003             *\n");
+    printf ("*             Patipan-Jampasit          *\n");
+    printf ("*\t\t\t        \"IOT\"               *\n");
+	printf ("*                                       *\n");
+    printf ("*****************************************\n");
+
+	getch(); //#include <conio.h>
+
+}
