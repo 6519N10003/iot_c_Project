fa31e5e3ae7111585f9582c02c657d915e5436fb
6519N10003 <s6519N10003@sau.ac.th>
2023-06-15 16:10:28 +0700

    Version 3........

--- practice01/pt02.c
+++ practice01/pt02.c
@@ -1,6 +1,20 @@
-//โจทย์ข้อที่1
+#include <stdio.h>
+#include <conio.h>
 
-viid main(){
+#define PA printf ("++++++++++++++++++++++++++++++++++++++++++\n") ;
 
+void main()
+{
+    float 
+    PA
+    printf ("Program Area Shape\n") ;
+    PA
+    printf ("Input Widhts :") ;
+    scanf("%f",&widths);
+    printf ("Input Longs :") ;
+    scanf("%f",&longs);
+    area_square = widths * longs ;
+    printf ("Square Area = %.2f\n", area_square) ;
     
-}
+    PA
+    getch ();
